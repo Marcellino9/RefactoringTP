5fad3e487eda0b7d3398b0f372d69c075b58dfb6
Dazou <jeanmarcellino25@gmail.com>
2025-08-13 14:17:35 +0300

    modiffication de la méthode ami dans la classe Humain

--- RefactoringTP/src/iut/bad/Femme.java
+++ RefactoringTP/src/iut/bad/Femme.java
@@ -4,4 +4,10 @@ public class Femme extends Humain {
     public Femme(String nom, String prenom, int age) {
 		super(nom, prenom, age);
 	}
+    public static void main(String[] args) {
+        Homme h = new Homme("Dupont", "Jean", 30);
+        Femme f = new Femme("Martin", "Sophie", 28);
+
+        h.ami(f); // amitié simple
+    }
 }
