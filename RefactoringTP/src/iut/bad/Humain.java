d5a9810d1d4fc62846528e4f4cf65d857e29b5e6
Dazou <jeanmarcellino25@gmail.com>
2025-08-13 12:53:29 +0300

    Création de la classe Humain.

---
+++ RefactoringTP/src/iut/bad/Humain.java
@@ -0,0 +1,17 @@
+package iut.bad;
+
+public class Humain {
+	protected String nom;
+    protected String prenom;
+    protected int age;
+
+    public Humain(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return nom + " " + prenom + " (" + age + " ans)";
+    }
+}
